191967d4af51170796a1104fc2c9657e5fb893e3
David <davidaznar366@gmail.com>
2017-11-08 18:22:42 +0100

    Projecte cotxe de David per a Brenda

---
+++ coche/src/model/Coche.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author 100005543.joan23
+ */
+public class Coche {
+    String nombre;
+    int telefono;
+    String color;
+    String model;
+    String opciones;
+
+    public Coche() {
+    }
+
+    public Coche(String nombre, int telefono, String color, String model, String opciones) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.color = color;
+        this.model = model;
+        this.opciones = opciones;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getOpciones() {
+        return opciones;
+    }
+
+    public void setOpciones(String opciones) {
+        this.opciones = opciones;
+    }
+    
+    
+    
+    
+    
+}
